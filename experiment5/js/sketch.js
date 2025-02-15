8e9ee22b644cc3492f6ed828b7f62530c7c99508
William Klunder <82077867+Klundyy@users.noreply.github.com>
2025-02-14 22:05:42 -0800

    random point spawning wave

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -6,13 +6,8 @@
 // Note that p5.js looks for a file called sketch.js
 
 // Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
 
 // Globals
-let myInstance;
 let canvasContainer;
 var centerHorz, centerVert;
 
@@ -20,29 +15,19 @@ let cols, rows;
 let spacing = 10;
 let w = 400;
 let h = 400;
-let heights = [];
-
-
-
-
+let origin;
 
 function resizeScreen() {
-  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
+  centerHorz = canvasContainer.width() / 2;
+  centerVert = canvasContainer.height() / 2;
   console.log("Resizing...");
   resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
 }
 
-// setup() function is called once when the program starts
 function setup() {
-  // place our canvas, making it fit our container
   canvasContainer = $("#canvas-container");
   let canvas = createCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
   canvas.parent("canvas-container");
-  // resize canvas is the page is resized
-
-  // create an instance of the class
 
   $(window).resize(function() {
     resizeScreen();
@@ -52,27 +37,23 @@ function setup() {
   angleMode(DEGREES);
   cols = w / spacing;
   rows = h / spacing;
-  
-  // Initialize height values to 0
-  for (let x = 0; x < cols; x++) {
-    heights[x] = [];
-    for (let y = 0; y < rows; y++) {
-      heights[x][y] = 0;
-    }
-  }
 
-  
+  // Random spawn point for wave
+  let randomX = floor(random(cols));
+  let randomY = floor(random(rows));
+  origin = createVector(randomX, randomY);
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
   background(0);
   rotateX(60);
-  rotateZ(frameCount/5);
-  translate(-w / 2, -h / 2); // Center the grid
+  rotateZ(frameCount / 5);
+  translate(-w / 2, -h / 2);
 
-  stroke(color(0,100,180));
+  stroke(color(0, 100, 180));
   noFill();
+
 
   // Draw grid as lines
   for (let y = 0; y < rows - 1; y++) {
@@ -80,12 +61,15 @@ function draw() {
     for (let x = 0; x < cols; x++) {
       let xPos = x * spacing;
       let yPos = y * spacing;
-      let zPos = sin(frameCount+x*20)*60;
+      let distFromOrigin = dist(x, y, origin.x, origin.y);
+      let zPos = sin(frameCount - distFromOrigin * 5) * 50;
+
       vertex(xPos, yPos, zPos);
     }
     endShape();
   }
 }
+
 
 
 
